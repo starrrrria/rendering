23b7667de407bacdca0321440474fe6a84b94c93
starrrrria <guok@tcd.ie>
2021-02-05 15:56:36 +0000

    Initial Commit

---
+++ rendering/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rendering
+//
+//  Created by 补锌 on 2021/2/5.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
